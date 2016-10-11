212af689ccd18d80f1ac7c69dd49cd1fe22211a4
int0h <int0h@yandex.ru>
2016-10-12 01:35:50 +0400

    refactor

--- tests/fg-src/api_basic/simpleClass/class.ts
+++ tests/fg-src/api_basic/simpleClass/class.ts
@@ -1,5 +1,5 @@
-// import {Component} from 'fg-js';
+import {Component} from 'fg-js/build/client/main';
 
-// export class TestClass extends Component{
+export class TestClass extends Component{
 
-// }; 
+}; 
